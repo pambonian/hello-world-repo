da711a3885ba120263132f222957441641bfdd1f
pambonian <pambonian@gmail.com>
2021-12-15 16:56:53 -0500

    react app lesson start

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+const student = {
+  name: 'Bob',
+  class: 'Software Engineering 700',
+  semester: 'Fall 2022'
+}
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +24,14 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <h1>{student.name}</h1>
+        <p>{student.class}</p>
+        <p>{student.semester}</p>
+      </div>
+      <div>
+        {(student.name === 'Cameron' ? student.class = 'Art' : student.class = 'SEI')}
+      </div>
     </div>
   );
 }
