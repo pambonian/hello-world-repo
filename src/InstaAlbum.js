9ff4c8be4e55d0bee652a1ac746190188bc09196
pambonian <pambonian@gmail.com>
2021-12-16 18:42:49 -0500

    tests added

--- src/InstaAlbum.js
+++ src/InstaAlbum.js
@@ -7,7 +7,7 @@ import './InstaAlbum.css';
 
 const post = {
     caption: 'I love coding. All day everyday....',
-    username: '@Patrick Brennan'
+    username: '@PatrickBrennan'
 }
 
 const postList = [
@@ -151,9 +151,76 @@ class InstaAlbum extends Component {
                                             <img src="https://source.unsplash.com/random/96x96" alt="Placeholder image" />
                                         </figure>
                                     </div>
+                                    <div className="media-content" data-testid="blurb">
+                                        <p className="title is-4">{post.caption}</p>
+                                        <p className="subtitle is-6">{post.username}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card-image">
+                                <figure className="image is-4by3">
+                                    <img src="https://source.unsplash.com/random/1280x960" alt="Placeholder image" />
+                                </figure>
+                            </div>
+                            <div className="card-content">
+                                <div className="level is-mobile">
+                                    <div className="level-left">
+                                        <div className="level-item has-text-centered">
+                                            <a href="">
+                                                <i className="material-icons">favorite_border</i>
+                                            </a>
+                                        </div>
+                                        <div className="level-item has-text-centered">
+                                            <div>
+                                                <a href="">
+                                                    <i className="material-icons">chat_bubble_outline</i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="content">
+                                    <p>
+                                        <strong title="zoinks">33 Likes</strong>
+                                    </p>
+                                    Lorem ipsum dolor sit amet, consectetur dogma adipiscing elit. Phasellus nec iaculis mauris.
+                                    <a>@bulmaio</a>.
+                                    <a href="#">#css</a>
+                                    <a href="#">#sustainable</a>
+                                    <br>
+                                    </br>
+                                    <time dateTime="2018-1-1">11:09 PM - 1 Jan 2018</time>
+                                </div>
+                            </div>
+                            <div className="card-footer">
+                                <div className="columns is-mobile">
+                                    <div className="column is-11">
+                                        <div className="field">
+                                            <div className="control">
+                                                <input className="input is-medium" type="text" placeholder="Add a comment . . ." />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="column has-text-centered">
+                                        <button className="button">
+                                            <i className="material-icons">more_horiz</i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="header">
+                                <div className="media">
+                                    <div className="media-left">
+                                        <figure className="image is-48x48">
+                                            <img src="https://source.unsplash.com/random/96x96" alt="Placeholder image" />
+                                        </figure>
+                                    </div>
                                     <div className="media-content">
-                                        <p className="title is-4">{post.message}</p>
-                                        <p className="subtitle is-6">{post.author}</p>
+                                        <p className="title is-4">{post.caption}</p>
+                                        <p className="subtitle is-6">{post.username}</p>
                                     </div>
                                 </div>
                             </div>
@@ -184,7 +251,7 @@ class InstaAlbum extends Component {
                                     <p>
                                         <strong>32 Likes</strong>
                                     </p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                                    Lorem ipsum dolor sit amet, cetazine consectetur adipiscing elit. Phasellus nec iaculis mauris.
                                     <a>@bulmaio</a>.
                                     <a href="#">#css</a>
                                     <a href="#">#responsive</a>
@@ -219,75 +286,8 @@ class InstaAlbum extends Component {
                                         </figure>
                                     </div>
                                     <div className="media-content">
-                                        <p className="title is-4">J{post.message}</p>
-                                        <p className="subtitle is-6">{post.author}</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="card-image">
-                                <figure className="image is-4by3">
-                                    <img src="https://source.unsplash.com/random/1280x960" alt="Placeholder image" />
-                                </figure>
-                            </div>
-                            <div className="card-content">
-                                <div className="level is-mobile">
-                                    <div className="level-left">
-                                        <div className="level-item has-text-centered">
-                                            <a href="">
-                                                <i className="material-icons">favorite_border</i>
-                                            </a>
-                                        </div>
-                                        <div className="level-item has-text-centered">
-                                            <div>
-                                                <a href="">
-                                                    <i className="material-icons">chat_bubble_outline</i>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="content">
-                                    <p>
-                                        <strong>32 Likes</strong>
-                                    </p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                                    <a>@bulmaio</a>.
-                                    <a href="#">#css</a>
-                                    <a href="#">#responsive</a>
-                                    <br>
-                                    </br>
-                                    <time dateTime="2018-1-1">11:09 PM - 1 Jan 2018</time>
-                                </div>
-                            </div>
-                            <div className="card-footer">
-                                <div className="columns is-mobile">
-                                    <div className="column is-11">
-                                        <div className="field">
-                                            <div className="control">
-                                                <input className="input is-medium" type="text" placeholder="Add a comment . . ." />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="column has-text-centered">
-                                        <button className="button">
-                                            <i className="material-icons">more_horiz</i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="header">
-                                <div className="media">
-                                    <div className="media-left">
-                                        <figure className="image is-48x48">
-                                            <img src="https://source.unsplash.com/random/96x96" alt="Placeholder image" />
-                                        </figure>
-                                    </div>
-                                    <div className="media-content">
-                                        <p className="title is-4">{post.message}</p>
-                                        <p className="subtitle is-6">{post.author}</p>
+                                        <p className="title is-4">{post.caption}</p>
+                                        <p className="subtitle is-6">{post.username}</p>
                                     </div>
                                 </div>
                             </div>
--- src/InstaAlbum.test.js
+++ src/InstaAlbum.test.js
@@ -19,6 +19,48 @@ test('renders specific caption', () => {
     expect(spanElement).toBeInTheDocument();
 });
 
+test('renders specific username', () => {
+    render(<InstaAlbum />);
+    const spanElement = screen.getByText(/@mike/i);
+    expect(spanElement).toBeInTheDocument();
+});
+
+test('renders search text in search bar', () => {
+    render(<InstaAlbum />);
+    const spanElement = screen.getByText(/search/i);
+    expect(spanElement).toBeInTheDocument();
+});
+
+test('renders license information at bottom of page', () => {
+    render(<InstaAlbum />);
+    const spanElement = screen.getByText(/The website content is licensed/i);
+    expect(spanElement).toBeInTheDocument();
+});
+
+test('renders CC info', () => {
+    render(<InstaAlbum />);
+    const spanElement = screen.getByText(/CC BY NC SA/i);
+    expect(spanElement).toBeInTheDocument();
+});
+
+test('renders specific caption', () => {
+    render(<InstaAlbum />);
+    const spanElement = screen.getByText(/dogma/i);
+    expect(spanElement).toBeInTheDocument();
+});
+
+test('renders specific testId', () => {
+    render(<InstaAlbum />);
+    const spanElement = screen.getByTestId('blurb');
+    expect(spanElement).toBeInTheDocument();
+});
+
+test('renders specific testId', () => {
+    render(<InstaAlbum />);
+    const spanElement = screen.getByTitle("zoinks");
+    expect(spanElement).toBeInTheDocument();
+});
+
 
 
 
